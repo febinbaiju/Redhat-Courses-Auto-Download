c30e50736e1da2d4180ec8035c4207161353070c
Febin Baiju <febin129@gmail.com>
2020-04-14 00:41:12 +0530

    extension loaded

--- driver.py
+++ driver.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+import time
 
 chrome_options = Options()
 chrome_options.add_argument("--user-data-dir=selenium")
-driver = webdriver.Chrome('./chromedriver',options=chrome_options)
+# chrome_options.add_extension('./HLS-Downloader_v1.7.2.crx')
+driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+driver.get('https://www.redhat.com/rhtapps/promo-rh024/?segment=0')
+element = driver.find_element_by_id('rhtk_player').click() # click the player to start streaming
 
+time.sleep(5)  # we need a delay before we can click the extension
