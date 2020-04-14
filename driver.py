9cd095fbd407331b909efb3798efa026f9808730
Febin Baiju <febin129@gmail.com>
2020-04-14 13:32:20 +0530

    file download (720p) done

--- driver.py
+++ driver.py
@@ -11,11 +11,11 @@ element = driver.find_element_by_id('rhtk_player').click() # click the player to
 
 time.sleep(5)  # we need a delay before we can click the extension
 import pyautogui
-#print(pyautogui.displayMousePosition())
+
 pyautogui.click(x=1236,y=51,clicks=1)
 time.sleep(5)
 arrows = pyautogui.locateOnScreen("./arrow.png")
 pyautogui.click(x=arrows[0],y=arrows[1],clicks=1,interval=0.0,button="left")
-print(arrows)
-hdselect = pyautogui.locateOnScreen("./arrow.png")
-print(hdselect)
+#print(pyautogui.displayMousePosition())
+
+pyautogui.click(x=1140,y=419,clicks=1) #downloading the file
