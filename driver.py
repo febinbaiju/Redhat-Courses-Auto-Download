ee7957adb6decc6b0f0fd4dc2151bb55052a434d
Febin Baiju <febin129@gmail.com>
2020-04-14 12:23:35 +0530

    extension click using pyautogui implemented

--- driver.py
+++ driver.py
@@ -10,3 +10,7 @@ driver.get('https://www.redhat.com/rhtapps/promo-rh024/?segment=0')
 element = driver.find_element_by_id('rhtk_player').click() # click the player to start streaming
 
 time.sleep(5)  # we need a delay before we can click the extension
+import pyautogui
+#print(pyautogui.displayMousePosition())
+pyautogui.click(x=1236,y=51,clicks=1)
+
